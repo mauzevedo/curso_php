d7a340cab217ab43cedc6173c46acaffc33d11cc
Mauzevedo <mauzevedo1979@gmail.com>
2020-07-16 11:46:42 -0300

    integracao

---
+++ basico/comentarios.php
@@ -0,0 +1,27 @@
+<div class="titulo">Comentários PHP</div>
+
+<?php
+echo "Estou no php";
+// echo "Comentário";
+# echo "Comentário";
+# echo // "Comentário";
+// echo # "Comentário";
+//?>
+
+<p>depois do primeiro bloco!</p>
+<!-- <p>Comentário HTML</p> -->
+<!-- 
+    <p>Comentário HTML 1 </p> 
+    <p>Comentário HTML 2 </p> 
+    <p>Comentário HTML 3 </p> 
+-->
+
+<?php
+/*
+Linha 1 do comentário
+Linha 2 do comentário
+Linha 3 do comentário
+*/
+?>
+
+<p>depois do primeiro bloco!</p>
